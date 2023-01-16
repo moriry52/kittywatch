7bfc88ad8098ca10716da1748a838e8e0985dcf3
unknown <moriry52@gmail.com>
2023-01-16 20:46:55 +0900

    forth

--- node/app.js
+++ node/app.js
@@ -3,6 +3,10 @@ const iconv = require("iconv-lite");
 
 exports.handler = async (event, context) => {
 	const pathname = event.queryStringParameters.cors;
+	return {
+		statusCode: 422,
+		body: pathname,
+	}
 	console.log(pathname)
 	if (event.httpMethod == 'GET') {
 		if (pathname == "holidays") {
