b5d6fcf5162df0bc6e42876c1e18f7b61cab645f
unknown <moriry52@gmail.com>
2023-01-16 21:01:35 +0900

    forth

--- node/app.js
+++ node/app.js
@@ -3,28 +3,25 @@ const iconv = require("iconv-lite");
 
 exports.handler = async (event, context) => {
 	const pathname = event.queryStringParameters.cors;
-	console.log(pathname)
 	if (event.httpMethod == 'GET') {
 		if (pathname == "holidays") {
 			const res = await get('https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv').catch(error => {
 				return {
 					statusCode: 404,
-					body: `Error: ${error}`,
+					body: `Error: ${error}`
 				}
 			})
 			const now = new Date();
 			const today = new Date(`${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`).getTime();
 			let holidays = iconv.decode(res.contents, "Shift_JIS").split('\r\n').slice(1).map(value => value.split(',')).filter(value => new Date(value[0]).getTime() >= today);
-			console.log(holidays)
 			return {
 				statusCode: 200,
-				headers: {'Content-Type': 'application/json' },
+				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify(holidays)
 			}
 
 		} else {
 			const res = await get(pathname).catch(error => {
-				console.log(error)
 				return {
 					statusCode: 404,
 					body: `Error: ${error}`,
